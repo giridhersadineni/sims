2075f7155d23cd49d5440aa63bec837887f45748
skillverse <skillverseit@gmail.com>
2018-06-20 16:53:33 +0530

    added nav

--- nav.php
+++ nav.php
@@ -4,8 +4,8 @@
                         <li class="nav-label">Home</li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Dashboard </span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="index.html">Ecommerce </a></li>
-                                <li><a href="index1.html">Analytics </a></li>
+                                <li><a href="index.php">Ecommerce </a></li>
+                                <li><a href="index.php">Analytics </a></li>
                             </ul>
                         </li>
                         <li class="nav-label">Employee</li>
@@ -18,18 +18,18 @@
                         </li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-male"></i></i><span class="hide-menu">Enquiries</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="chart-flot.html"><i class="fa fa-user-plus"></i> Add Enquiry</a></li>
-                                <li><a href="chart-morris.html"><i class="fa fa-table"></i> Enquiry Report</a></li>
-                                <li><a href="chart-chartjs.html"><i class="fa fa-globe"></i> Online Enquiries</a></li>
+                                <li><a href="addenquiry.php"><i class="fa fa-user-plus"></i> Add Enquiry</a></li>
+                                <li><a href="#"><i class="fa fa-table"></i> Enquiry Report</a></li>
+                                <li><a href="#"><i class="fa fa-globe"></i> Online Enquiries</a></li>
                                 
                             </ul>
                         </li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-phone-square"></i><span class="hide-menu">Contact</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="ui-alert.html"><i class="fa fa-user"></i>  Students</a></li>
-                                <li><a href="ui-button.html"><i class="fa fa-user"></i  >Employee</a></li>
-                                <li><a href="ui-dropdown.html"><i class="fa fa-envelope"></i>Email</a></li>
-                                <li><a href="ui-progressbar.html"><i class="fa fa-comment"></i> Bulk SMS</a></li>
+                                <li><a href="#"><i class="fa fa-user"></i>  Students</a></li>
+                                <li><a href="#"><i class="fa fa-user"></i  >Employee</a></li>
+                                <li><a href="#"><i class="fa fa-envelope"></i>Email</a></li>
+                                <li><a href="#"><i class="fa fa-comment"></i> Bulk SMS</a></li>
                             </ul>
                         </li>
 						</ul>
